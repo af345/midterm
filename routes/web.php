7d5f4e9fe3b779780da2b6447c1dd5e2bea9b48c
alexandraflores <af345@njit.edu>
2018-10-31 20:41:32 -0400

    Create a controller, route, and view to show an about page and add it to the main menu of your site.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -10,11 +9,16 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::get('/', 'PagesController@home') ->name('home');
+Route::get('/about', 'PagesController@about')->name('about');
+Route::get('/contact', 'PagesController@contact')->name('contact');
+Route::post('/contact', 'PagesController@store')->name('contact.store');
+Route::get('/thanks/{name}', 'PagesController@thanks')->name('thanks');
+/*Route::post('/contact', function () {
+    $data = request() ->all();
+    echo "Name: " . $data['name']. '<br>';
+    echo "Email: " . $data['email']. '<br>';
+    echo "Message: " . $data['body'];
+});*/
 Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
